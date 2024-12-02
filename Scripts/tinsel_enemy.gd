a7766dd2c281ca8c66f0a781f002f4acc6cde995
Bro-dep <153253648+Bro-dep@users.noreply.github.com>
2024-12-01 23:57:32 -0800

    made awake

--- Scripts/tinsel_enemy.gd
+++ Scripts/tinsel_enemy.gd
@@ -5,18 +5,32 @@ const SPEED = 20
 
 
 var direction = -1
+var awake
 
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var awakeTimer = $Timer
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if ray_cast_right.is_colliding():
-		direction = -1
-		animated_sprite.flip_h = false
-	if ray_cast_left.is_colliding():
-		direction = 1
-		animated_sprite.flip_h = true
+	if awake:
+		animated_sprite.play("Move")
+		if ray_cast_right.is_colliding():
+			direction = -1
+			animated_sprite.flip_h = false
+		if ray_cast_left.is_colliding():
+			direction = 1
+			animated_sprite.flip_h = true
 	
-	position.x += direction * SPEED * delta
+		position.x += direction * SPEED * delta
 	
+
+
+func _on_sensor_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		animated_sprite.play("Awake")
+		awakeTimer.start()
+
+
+func _on_timer_timeout() -> void:
+	awake = true
